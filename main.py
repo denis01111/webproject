89e5345aaea9d0e3acad348eb9034a5a30703967
First commit <denis_makartsev@mail.ru>
2020-04-06 18:56:17 +0300

    First-Denis

--- main.py
+++ main.py
@@ -1,3 +1,61 @@
 from data import db_session
+from data import db_session, users
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+from flask import Flask, request, render_template, redirect
+from flask_login import LoginManager, login_user
 
-db_session.global_init("db/blogs.sqlte")
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+def main():
+    app.run()
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    session = db_session.create_session()
+    return session.query(users.User).get(user_id)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    try:
+        session = db_session.create_session()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        session = db_session.create_session()
+        if not session:
+            return 'Данного пользователя не существует!'
+        user = session.query(users.User).filter(users.User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    main()
