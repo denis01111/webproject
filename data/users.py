4547eb4819d5306669086ac057dceb1f7921a9d1
First commit <denis_makartsev@mail.ru>
2020-04-09 16:25:14 +0300

    Исправил ошибку с Авторизацией(Денис)

--- data/users.py
+++ data/users.py
@@ -1,19 +1,27 @@
 import datetime
 import sqlalchemy
-import sqlalchemy.ext.declarative as dec
+from sqlalchemy import orm
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from .db_session import SqlAlchemyBase
+from sqlalchemy_serializer import SerializerMixin
 
-SqlAlchemyBase = dec.declarative_base()
 
-
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer,
-                           primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String,
-                              index=True, unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
-                                     default=datetime.datetime.now)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
+    password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    city = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+    def __repr__(self):
+        return f'{self.id}, {self.name}, {self.about}, {self.email}'
--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-from data import db_session, users
+from flask import Flask, render_template, redirect, request, make_response, session, abort, jsonify
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, BooleanField
 from wtforms.validators import DataRequired
-from flask import Flask, request, render_template, redirect
-from flask_login import LoginManager, login_user
-db_session.global_init("db/blogs.sqlite")
+from data import db_session, users
+from loginform import LoginForm
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-def main():
-    app.run()
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
-    submit = SubmitField('Войти')
-
-
 @login_manager.user_loader
 def load_user(user_id):
-    session = db_session.create_session()
-    return session.query(users.User).get(user_id)
+    sessions = db_session.create_session()
+    return sessions.query(users.User).get(user_id)
 
 
 @app.route('/')
@@ -43,16 +31,18 @@ def delete():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        session = db_session.create_session()
-        print(users.User.email, users.User, form.email.data)
-        user = session.query(users.User).filter(users.User.email == form.email.data).first()
-        if user and user.check_password(form.password.data):
+        sessions = db_session.create_session()
+        user = sessions.query(users.User).filter(users.User.email == form.email.data).first()
+        if user and user.password == form.password.data:
             login_user(user, remember=form.remember_me.data)
-            return redirect("/")
-        return render_template('login.html',
-                               message="Неправильный логин или пароль",
-                               form=form)
+            return redirect('/')
+        return render_template('login.html', message='Неправильный логин или пароль', form=form)
     return render_template('login.html', title='Авторизация', form=form)
+
+
+def main():
+    db_session.global_init('db/blogs.sqlite')
+    app.run()
 
 
 if __name__ == '__main__':
